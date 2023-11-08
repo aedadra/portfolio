5f003997672d27266a7d61890b297538b196f17d
Clement Moulin <clement.moulin@yahoo.fr>
2023-11-08 22:14:57 +0100

    ajout de page erreur

---
+++ src/pages/error.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import "../styles/error.scss"
+
+function Error () {
+    return (
+        <div className="error">
+            <h1>404</h1>
+            <h3>oups, une erreur est survenue</h3>
+            <p>on retourne à la page d'accueil ?</p>
+            <Link to='/portfolio'>C'est par ici</Link>
+        </div>
+    )
+}
+
+export default Error
