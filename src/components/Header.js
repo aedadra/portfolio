49a07a1a976cb9bf3f02b1d7b541f379c5867b97
Clement Moulin <clement.moulin@yahoo.fr>
2023-11-08 21:36:34 +0100

    modification des routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,12 +8,12 @@ function Header() {
             <div className='header'>
                 <NavLink className={({ isActive, isPending }) =>
                     isPending ? "pending" : isActive ? "activeH1" : "pendingH1"
-                } to="/"><h1>Clément Moulin</h1></NavLink>
+                } to="/portfolio"><h1>Clément Moulin</h1></NavLink>
                 <div>
                     <nav className='header__link'>
                         <NavLink className={({ isActive, isPending }) =>
                             isPending ? "pending" : isActive ? "active" : "pending"
-                        } to="../pages/competences"><p>Mes Competences</p></NavLink>
+                        } to="/pages/competences"><p>Mes Competences</p></NavLink>
                         <NavLink className={({ isActive, isPending }) =>
                             isPending ? "pending" : isActive ? "active" : "pending"
                         } to="../pages/realisations"><p>Mes Réalisations</p></NavLink>
