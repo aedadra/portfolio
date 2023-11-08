b783f1072b1fc589871d2e5d1952ddad8e296b29
Clement Moulin <clement.moulin@yahoo.fr>
2023-11-09 00:06:25 +0100

    on va reussir a regler ce probleme de routes...

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import {HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/Header';
 import Project from './pages/realisations';
@@ -11,16 +11,16 @@ import Error from './pages/error';
 function App () {
   return (
 
-<div>
+<HashRouter>
       <Header />
       <Routes>
         <Route path= "#/portfolio" element={<Home />} />
-        <Route path="#/competences" element={<Competences />} />
+        <Route path="#/pages/competences" element={<Competences />} />
         <Route path="#/realisations" element={<Project />} />
         <Route path="#/realisationsDetails/:id" element={<ProjectDetails/>} />
         <Route path="*" element={<Error />} />
       </ Routes>
       <Footer />
-</div>
+</HashRouter>
 )}
 export default App
