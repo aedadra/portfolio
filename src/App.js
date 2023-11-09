8ce11a038b45f72d86a63a52e47ed30c6483ee0b
Clement Moulin <clement.moulin@yahoo.fr>
2023-11-09 01:01:42 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App () {
 <>
       <Header />
       <Routes>
-        <Route path= "portfolio" element={<Home />} />
-        <Route path="competences" element={<Competences />} />
+        <Route path= "." element={<Home />} />
+        <Route path="pages/competences" element={<Competences />} />
         <Route path="realisations" element={<Project />} />
         <Route path="realisationsDetails/:id" element={<ProjectDetails/>} />
         <Route path="*" element={<Error />} />
